e38ddc1ca022e22626d53dda017caa1415af02ee
Sebastien LANGE <sebastien.lange@syleam.fr>
2016-05-25 20:37:16 +0200

    [IMP] Change format page in landscape

--- wizard/account_report_partner_ledger.py
+++ wizard/account_report_partner_ledger.py
@@ -29,11 +29,17 @@ from openerp import api, fields, models
 class AccountPartnerLedger(models.TransientModel):
     _inherit = "account.report.partner.ledger"
 
-    partner_ids = fields.Many2many(comodel_name='res.partner', string='Partners', domain=['|',('is_company','=',True),('parent_id','=',False)], help='If empty, get all partners')
+    partner_ids = fields.Many2many(comodel_name='res.partner', string='Partners', domain=['|', ('is_company', '=', True), ('parent_id', '=', False)], help='If empty, get all partners')
 
     @api.multi
     def pre_print_report(self, data):
         data['form'].update({'partner_ids': self.partner_ids.mapped('id')})
         return super(AccountPartnerLedger, self).pre_print_report(data)
+
+    # FIXME : find an other solution to pass context instead of rewrite this code
+    def _print_report(self, data):
+        data = self.pre_print_report(data)
+        data['form'].update({'reconciled': self.reconciled, 'amount_currency': self.amount_currency})
+        return self.env['report'].with_context(landscape=True).get_action(self, 'account_extra_reports.report_partnerledger', data=data)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
