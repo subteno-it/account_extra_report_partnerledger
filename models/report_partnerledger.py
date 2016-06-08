28ed95824e2d3d83043993044e5d76322bbe3c7f
Sebastien LANGE <sebastien.lange@syleam.fr>
2016-06-08 09:25:05 +0200

    [IMP] Add initial balance and review report presentation

--- models/report_partnerledger.py
+++ models/report_partnerledger.py
@@ -59,58 +59,115 @@ class ReportPartnerLedger(models.AbstractModel):
             full_account.append(r)
         return full_account
 
+    def _sum_partner(self, data, partner, field):
+        if field not in ['debit', 'credit', 'debit - credit']:
+            return
+        result = 0.0
+        query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}), date_from=False)._query_get()
+
+        params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
+        query = """SELECT sum(""" + field + """)
+                FROM """ + query_get_data[0] + """, account_move AS m
+                WHERE "account_move_line".partner_id = %s
+                    AND m.id = "account_move_line".move_id
+                    AND m.state IN %s
+                    AND account_id IN %s
+                    AND """ + query_get_data[1]
+        self.env.cr.execute(query, tuple(params))
+
+        contemp = self.env.cr.fetchone()
+        if contemp is not None:
+            result = contemp[0] or 0.0
+        return result
+
+    def _initial_balance_partner(self, data, partner, field):
+        if field not in ['debit', 'credit', 'debit - credit']:
+            return
+        result = 0.0
+        query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}), initial_bal=True, date_to=False)._query_get()
+
+        params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
+        query = """SELECT sum(""" + field + """)
+                FROM """ + query_get_data[0] + """, account_move AS m
+                WHERE "account_move_line".partner_id = %s
+                    AND m.id = "account_move_line".move_id
+                    AND m.state IN %s
+                    AND account_id IN %s
+                    AND """ + query_get_data[1]
+        self.env.cr.execute(query, tuple(params))
+
+        contemp = self.env.cr.fetchone()
+        if contemp is not None:
+            result = contemp[0] or 0.0
+        return result
+
     @api.multi
     def render_html(self, data):
+        data['computed'] = {}
+
+        obj_partner = self.env['res.partner']
+        query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
+        data['computed']['move_state'] = ['draft', 'posted']
+        if data['form'].get('target_move', 'all') == 'posted':
+            data['computed']['move_state'] = ['posted']
+        result_selection = data['form'].get('result_selection', 'customer')
+        if result_selection == 'supplier':
+            data['computed']['ACCOUNT_TYPE'] = ['payable']
+        elif result_selection == 'customer':
+            data['computed']['ACCOUNT_TYPE'] = ['receivable']
+        else:
+            data['computed']['ACCOUNT_TYPE'] = ['payable', 'receivable']
+
+        self.env.cr.execute("""
+            SELECT a.id
+            FROM account_account a
+            WHERE a.internal_type IN %s
+            AND NOT a.deprecated""", (tuple(data['computed']['ACCOUNT_TYPE']),))
+        data['computed']['account_ids'] = [a for (a,) in self.env.cr.fetchall()]
+        params = [tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
+        reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
+        query = """
+            SELECT DISTINCT "account_move_line".partner_id
+            FROM """ + query_get_data[0] + """, account_account AS account, account_move AS am
+            WHERE "account_move_line".partner_id IS NOT NULL
+                AND "account_move_line".account_id = account.id
+                AND am.id = "account_move_line".move_id
+                AND am.state IN %s
+                AND "account_move_line".account_id IN %s
+                AND NOT account.deprecated
+                AND """ + query_get_data[1] + reconcile_clause
+        self.env.cr.execute(query, tuple(params))
+        partner_move_ids = [res['partner_id'] for res in self.env.cr.dictfetchall()]
         if data['form'].get('partner_ids'):
-            data['computed'] = {}
+            partner_ids = data['form'].get('partner_ids')
+        else:
+            query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}), initial_bal=True, date_to=False)._query_get()
 
-            obj_partner = self.env['res.partner']
-            query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
-            data['computed']['move_state'] = ['draft', 'posted']
-            if data['form'].get('target_move', 'all') == 'posted':
-                data['computed']['move_state'] = ['posted']
-            result_selection = data['form'].get('result_selection', 'customer')
-            if result_selection == 'supplier':
-                data['computed']['ACCOUNT_TYPE'] = ['payable']
-            elif result_selection == 'customer':
-                data['computed']['ACCOUNT_TYPE'] = ['receivable']
-            else:
-                data['computed']['ACCOUNT_TYPE'] = ['payable', 'receivable']
-
-            self.env.cr.execute("""
-                SELECT a.id
-                FROM account_account a
-                WHERE a.internal_type IN %s
-                AND NOT a.deprecated""", (tuple(data['computed']['ACCOUNT_TYPE']),))
-            data['computed']['account_ids'] = [a for (a,) in self.env.cr.fetchall()]
             params = [tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
-            reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
-            query = """
-                SELECT DISTINCT "account_move_line".partner_id
-                FROM """ + query_get_data[0] + """, account_account AS account, account_move AS am
-                WHERE "account_move_line".partner_id IS NOT NULL
-                    AND "account_move_line".account_id = account.id
-                    AND am.id = "account_move_line".move_id
-                    AND am.state IN %s
-                    AND "account_move_line".account_id IN %s
-                    AND NOT account.deprecated
-                    AND """ + query_get_data[1] + reconcile_clause
+            query = """SELECT DISTINCT "account_move_line".partner_id, sum(debit - credit) AS sum
+                    FROM """ + query_get_data[0] + """, account_move AS m
+                    WHERE m.id = "account_move_line".move_id
+                        AND m.state IN %s
+                        AND account_id IN %s
+                        AND """ + query_get_data[1] + """ GROUP BY "account_move_line".partner_id"""
             self.env.cr.execute(query, tuple(params))
-            partner_ids = list(set([res['partner_id'] for res in self.env.cr.dictfetchall()]) & set(data['form']['partner_ids']))
-            partners = obj_partner.browse(partner_ids)
-            partners = sorted(partners, key=lambda x: (x.ref, x.name))
+            partner_initial_move_ids = [res['partner_id'] for res in self.env.cr.dictfetchall() if float(res['sum'] or 0.) != 0.]
+            partner_ids = list(set(partner_move_ids) | set(partner_initial_move_ids))
 
-            docargs = {
-                'doc_ids': partner_ids,
-                'doc_model': self.env['res.partner'],
-                'data': data,
-                'docs': partners,
-                'time': time,
-                'lines': self._lines,
-                'sum_partner': self._sum_partner,
-            }
-            return self.env['report'].render('account_extra_reports.report_partnerledger', docargs)
-        return super(ReportPartnerLedger, self).render_html(data)
+        partners = obj_partner.browse(partner_ids)
+        partners = sorted(partners, key=lambda x: (x.ref, x.name))
+
+        docargs = {
+            'doc_ids': partner_ids,
+            'doc_model': self.env['res.partner'],
+            'data': data,
+            'docs': partners,
+            'time': time,
+            'lines': self._lines,
+            'sum_partner': self._sum_partner,
+            'initial_balance_partner': self._initial_balance_partner,
+        }
+        return self.env['report'].render('account_extra_reports.report_partnerledger', docargs)
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
