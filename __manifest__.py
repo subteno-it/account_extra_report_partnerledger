5f748282da3fcb81a0743c925825590dcd372d1a
fmdl <florent.mirieu@gmail.com>
2017-02-03 23:33:56 +0100

    [MIG] make module installable
    TODO :
    - fix wizard view
    - check the validity of return data

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Extra Accounting Report Partner Ledger',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'description': """
+Extra Accounting Report Partner Ledger.
+====================================
+
+This module modify report:
+* Partner Ledger
+    """,
+    'website': 'www.syleam.com',
+    'depends': [
+    ],
+    'data': [
+        'views/report_partnerledger.xml',
+        'wizard/account_report_partner_ledger_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ wizard/account_report_partner_ledger.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    account_extra_report_partnerledger module for OpenERP, Review report partnerledger from account_extra_reports
+#    Copyright (C) 2016 SYLEAM Info Services (<http://www.syleam.fr>)
+#              Sebastien LANGE <sebastien.lange@syleam.fr>
+#
+#    This file is a part of account_extra_report_partnerledger
+#
+#    account_extra_report_partnerledger is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    account_extra_report_partnerledger is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from odoo import api, fields, models
+
+class AccountPartnerLedger(models.TransientModel):
+    _inherit = "account.common.partner.report"
+    _name = "account.report.partner.ledger"
+    _description = "Account Partner Ledger"
+
+    amount_currency = fields.Boolean("With Currency", help="It adds the currency column on report if the currency differs from the company currency.")
+    reconciled = fields.Boolean('Reconciled Entries')
+
+    partner_ids = fields.Many2many(comodel_name='res.partner', string='Partners', domain=['|', ('is_company', '=', True), ('parent_id', '=', False)], help='If empty, get all partners')
+    account_exclude_ids = fields.Many2many(comodel_name='account.account', string='Accounts to exclude', domain=[('internal_type', 'in', ('receivable', 'payable'))], help='If empty, get all accounts')
+
+    @api.multi
+    def pre_print_report(self, data):
+        data['form'].update({
+            'partner_ids': self.partner_ids.mapped('id'),
+            'account_exclude_ids': self.account_exclude_ids.mapped('id'),
+        })
+        return super(AccountPartnerLedger, self).pre_print_report(data)
+
+    # FIXME : find an other solution to pass context instead of rewrite this code
+    def _print_report(self, data):
+        data = self.pre_print_report(data)
+        data['form'].update({'reconciled': self.reconciled, 'amount_currency': self.amount_currency})
+        return self.env['report'].with_context(landscape=True).get_action(self, 'account_extra_report_partnerledger.report_partnerledger', data=data)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
