6f5572a0ebe8bdfaa90fff78c552ca70b7288098
fmdl <florent.mirieu@gmail.com>
2017-03-08 00:45:19 +0100

    [IMP] add option to remove futur matching number

--- report/account_partner_ledger.py
+++ report/account_partner_ledger.py
@@ -12,27 +12,34 @@ class ReportPartnerLedger(models.AbstractModel):
     def _lines(self, data, partner):
         full_account = []
         query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
-        reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
+        reconcile_clause = data['reconcile_clause']
         params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
         query = """
-            SELECT "account_move_line".id, "account_move_line".date, "account_move_line".date_maturity, j.code, acc.code as a_code, acc.name as a_name, "account_move_line".ref, m.name as move_name, "account_move_line".name, "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,"account_move_line".currency_id, c.symbol AS currency_code, afr.name as "matching_number"
+            SELECT "account_move_line".id, "account_move_line".date, "account_move_line".date_maturity, j.code, acc.code as a_code, acc.name as a_name, "account_move_line".ref, m.name as move_name, "account_move_line".name, "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,"account_move_line".currency_id, c.symbol AS currency_code, afr.name as "matching_number", afr_id.id as "matching_number_id"
             FROM """ + query_get_data[0] + """
             LEFT JOIN account_journal j ON ("account_move_line".journal_id = j.id)
             LEFT JOIN account_account acc ON ("account_move_line".account_id = acc.id)
             LEFT JOIN res_currency c ON ("account_move_line".currency_id=c.id)
             LEFT JOIN account_move m ON (m.id="account_move_line".move_id)
             LEFT JOIN account_full_reconcile afr ON (afr.id="account_move_line".full_reconcile_id)
+            LEFT JOIN account_full_reconcile afr_id ON (afr_id.id="account_move_line".full_reconcile_id)
             WHERE "account_move_line".partner_id = %s
                 AND m.state IN %s
                 AND "account_move_line".account_id IN %s AND """ + query_get_data[1] + reconcile_clause + """
                 ORDER BY "account_move_line".date"""
         self.env.cr.execute(query, tuple(params))
         res = self.env.cr.dictfetchall()
-        sum = 0.0
+        balance = 0.0
         lang_code = self.env.context.get('lang') or 'en_US'
         lang = self.env['res.lang']
         lang_id = lang._lang_get(lang_code)
         date_format = lang_id.date_format
+
+        date_to = False
+        if data['form']['date_to']:
+            date_to = datetime.strptime(data['form']['date_to'], DEFAULT_SERVER_DATE_FORMAT)
+        acc_ful_obj = self.env['account.full.reconcile']
+
         for r in res:
             r['date'] = datetime.strptime(r['date'], DEFAULT_SERVER_DATE_FORMAT).strftime(date_format)
             r['date_maturity'] = datetime.strptime(r['date_maturity'], DEFAULT_SERVER_DATE_FORMAT).strftime(date_format)
@@ -40,8 +47,20 @@ class ReportPartnerLedger(models.AbstractModel):
                 r[field_name] for field_name in ('move_name', 'ref', 'name')
                 if r[field_name] not in (None, '', '/')
             )
-            sum += r['debit'] - r['credit']
-            r['progress'] = sum
+
+            if data['form']['rem_futur_reconciled'] and date_to:
+                in_futur = False
+                full_rec = acc_ful_obj.browse(r['matching_number_id'])
+                for date in full_rec.reconciled_line_ids.mapped('date_maturity'):
+                    date_move = datetime.strptime(date, DEFAULT_SERVER_DATE_FORMAT)
+                    if date_move > date_to:
+                        in_futur = True
+                        break
+                if in_futur:
+                    r['matching_number'] = '*'
+
+            balance += r['debit'] - r['credit']
+            r['progress'] = balance
             full_account.append(r)
         return full_account
 
@@ -50,7 +69,7 @@ class ReportPartnerLedger(models.AbstractModel):
             return
         result = 0.0
         query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
-        reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
+        reconcile_clause = data['reconcile_clause']
 
         params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
         query = """SELECT sum(""" + field + """)
@@ -92,7 +111,7 @@ class ReportPartnerLedger(models.AbstractModel):
                 AND NOT a.deprecated""", (tuple(data['computed']['ACCOUNT_TYPE']),))
             data['computed']['account_ids'] = [a for (a,) in self.env.cr.fetchall()]
             params = [tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
-            reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
+            reconcile_clause = data['reconcile_clause']
             query = """
                 SELECT DISTINCT "account_move_line".partner_id
                 FROM """ + query_get_data[0] + """, account_account AS account, account_move AS am
@@ -141,7 +160,7 @@ class ReportPartnerLedger(models.AbstractModel):
                 AND NOT a.deprecated""", (tuple(data['computed']['ACCOUNT_TYPE']),))
             data['computed']['account_ids'] = [a for (a,) in self.env.cr.fetchall()]
             params = [tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
-            reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
+            reconcile_clause = data['reconcile_clause']
             query = """
                 SELECT DISTINCT "account_move_line".partner_id
                 FROM """ + query_get_data[0] + """, account_account AS account, account_move AS am
--- wizard/account_report_partner_ledger.py
+++ wizard/account_report_partner_ledger.py
@@ -22,8 +22,10 @@
 #
 ##############################################################################
 
-
+from datetime import datetime
 from odoo import api, fields, models
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+
 
 class AccountPartnerLedger(models.TransientModel):
     _inherit = "account.common.partner.report"
@@ -32,21 +34,50 @@ class AccountPartnerLedger(models.TransientModel):
 
     amount_currency = fields.Boolean("With Currency", help="It adds the currency column on report if the currency differs from the company currency.")
     reconciled = fields.Boolean('With Reconciled Entries')
+    rem_futur_reconciled = fields.Boolean('Without futur matching number', default=True)
     partner_ids = fields.Many2many(comodel_name='res.partner', string='Partners', domain=['|', ('is_company', '=', True), ('parent_id', '=', False)], help='If empty, get all partners')
     account_exclude_ids = fields.Many2many(comodel_name='account.account', string='Accounts to exclude', domain=[('internal_type', 'in', ('receivable', 'payable'))], help='If empty, get all accounts')
 
     @api.multi
     def pre_print_report(self, data):
-        data['form'].update({
-            'partner_ids': self.partner_ids.mapped('id'),
-            'account_exclude_ids': self.account_exclude_ids.mapped('id'),
-        })
+        data['form'].update({'partner_ids': self.partner_ids.mapped('id'),
+                             'account_exclude_ids': self.account_exclude_ids.mapped('id'),
+                             })
         return super(AccountPartnerLedger, self).pre_print_report(data)
 
     # FIXME : find an other solution to pass context instead of rewrite this code
     def _print_report(self, data):
         data = self.pre_print_report(data)
-        data['form'].update({'reconciled': self.reconciled, 'amount_currency': self.amount_currency})
+        data['form'].update({'reconciled': self.reconciled,
+                             'rem_futur_reconciled': self.rem_futur_reconciled,
+                             'amount_currency': self.amount_currency
+                             })
+        data['reconcile_clause'] = self._compute_reconcile_clause(data)
+
         return self.env['report'].with_context(landscape=True).get_action(self, 'account_extra_report_partnerledger.report_partnerledger', data=data)
+
+    def _compute_reconcile_clause(self, data):
+        reconcile_clause = ""
+        if not data['form']['reconciled']:
+            reconcile_clause = ' AND "account_move_line".reconciled = false '
+
+        if not data['form']['reconciled'] and data['form']['rem_futur_reconciled'] and data['form']['date_to']:
+            date_to = datetime.strptime(data['form']['date_to'], DEFAULT_SERVER_DATE_FORMAT)
+            acc_ful_obj = self.env['account.full.reconcile']
+            in_futur = False
+            list_match_in_futur = []
+            for full_rec in acc_ful_obj.search([]):
+                for date in full_rec.reconciled_line_ids.mapped('date_maturity'):
+                    date_move = datetime.strptime(date, DEFAULT_SERVER_DATE_FORMAT)
+                    if date_move > date_to:
+                        in_futur = True
+                        break
+                if in_futur:
+                    list_match_in_futur.append(full_rec.id)
+
+            if list_match_in_futur:
+                reconcile_clause = """ AND ("account_move_line".reconciled = false OR "account_move_line".full_reconcile_id IN """ + str(tuple(list_match_in_futur)) + """)"""
+        return reconcile_clause
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
